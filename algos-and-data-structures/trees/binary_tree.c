8b63fc6180bca5426a94c0c05702ba35f13d8e24
Nicolas.Beland <nicolasb565@hotmail.com>
2019-11-11 21:55:46 -0500

    started working on binary tree

---
+++ algos-and-data-structures/trees/binary_tree.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "binary_tree.h"
+
+static binary_tree_node_t binary_tree_insert_internal(binary_tree_node_t node, void* data, int remaining_depth){
+    assert(node != NULL);
+    if(node->data == NULL){
+        node->data = data;
+        return node;
+    }
+    else if(remaining_depth < 1){ 
+        return NULL; 
+    }
+    else if(node->left == NULL){
+        if(node->left = malloc(sizeof(struct binary_tree_node))){
+            node->left->data = data;
+            node->left->left = NULL;
+            node->left->right = NULL;
+        }
+        return node->left;
+    }
+    else if(node->right == NULL){
+        if(node->right = malloc(sizeof(struct binary_tree_node))){
+            node->right->data = data;
+            node->right->left = NULL;
+            node->right->right = NULL;
+        }
+        return node->right;
+    }
+    else{
+        binary_tree_node_t new_node;
+        if(new_node = binary_tree_insert_internal(node->left, data, remaining_depth - 1)){
+            return new_node;
+        }
+        else if(new_node = binary_tree_insert_internal(node->right, data, remaining_depth - 1)){
+            return new_node;
+        }
+        else{
+            return NULL;
+        }
+    }
+}
+
+static void binary_tree_balance(binary_tree_t root){
+    assert(root != NULL);
+    /*TODO!!!*/
+}
+
+binary_tree_node_t binary_tree_create(binary_tree_t root){
+    assert(root != NULL);
+    root->root_node.data = NULL;
+    root->root_node.left = NULL;
+    root->root_node.right = NULL;
+}
+
+binary_tree_node_t binary_tree_insert(binary_tree_t root, void* data){
+    assert(root != NULL);
+    binary_tree_node_t new_node;
+    if(new_node = binary_tree_insert_internal(&root->root_node, data, root->depth)){
+        return new_node;
+    }
+    else{
+        root->depth+=1;
+        new_node = binary_tree_insert_internal(&root->root_node, data, root->depth);
+        binary_tree_balance(root);
+        return new_node;
+    }
+}
+
+void printTree(binary_tree_t root){
+    /*TODO!!!*/
+}
+
+int main(){
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d = 3;
+    struct binary_tree root;
+    binary_tree_create(&root);
+    binary_tree_insert(&root, &a);
+    binary_tree_insert(&root, &b);
+    binary_tree_insert(&root, &c);
+    binary_tree_insert(&root, &d);
+    return 0;
+}
---
+++ algos-and-data-structures/trees/binary_tree.h
@@ -0,0 +1,18 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+struct binary_tree_node{
+    void* data;
+    struct binary_tree_node* left;
+    struct binary_tree_node* right;
+};
+typedef struct binary_tree_node* binary_tree_node_t;
+
+struct binary_tree{
+    struct binary_tree_node root_node;
+    int depth;
+};
+typedef struct binary_tree* binary_tree_t;
+
+binary_tree_node_t binary_tree_create(binary_tree_t root);
+binary_tree_node_t binary_tree_insert(binary_tree_t root, void* data);
+#endif
